c79d9bd41e48505045ed709fbd691446db2b633a
Tobias <tikado@web.de>
2021-07-11 19:14:39 +0200

    feat: add forest scripts finalized

---
+++ Assets/Scripts/BeeForest/StateMachine/States/FlowerUnseededState.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// In this state we just want to wait for a length of time, then transition back to idle
+/// Because IState is not inheriting from MonoBehaviour, we don't have access to Coroutines
+/// This means that we're going to have to implement our own simple timer system.
+/// </summary>
+
+namespace Flowers.State
+{
+    public class FlowerUnseededState : IState
+    {
+        /// <summary>
+        /// This State disables the withered flower and enables a dug out hole with a dirt pile in its position instead.
+        /// </summary>
+
+        #region Variables
+
+        FlowerStateMachine _flowerSM;
+
+        #endregion
+
+        #region Functions
+
+        // Specific components can be sent down from the State Machine Controller to the States.
+        public FlowerUnseededState(FlowerStateMachine flowerSM)
+        {
+            _flowerSM = flowerSM;
+        }
+
+        // This functions marks the start of a new State and is automatically called by the State Machine.
+        public void Enter()
+        {
+            // Disables flower object and enables dirt pile / dug out hole.
+            _flowerSM.ToggleGameObjects("DugOutHole", true);
+            _flowerSM.ToggleGameObjects("FlowerBody", false);
+        }
+
+        // This functions marks the end of the current State and is automatically called by the State Machine.
+        public void Exit()
+        {
+
+        }
+
+        // Allows simulation of FixedUpdate() method without a MonoBehaviour attached.
+        public void FixedTick()
+        {
+
+        }
+
+        // Allows simulation of Update() method without a MonoBehaviour attached.
+        public void Tick()
+        {
+
+        }
+
+
+        #endregion
+    }
+}
+
---
+++ Assets/Scripts/BeeForest/StateMachine/States/FlowerWitheredState.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+
+namespace Flowers.State
+{
+    public class FlowerWitheredState : IState
+    {
+        /// <summary>
+        /// This is the initial state of the flower.
+        /// This State switches from a colorful flower material to a dark gray to simulate the withered flower.
+        /// </summary>
+
+        #region Variables
+
+        private FlowerStateMachine _flowerSM;
+        private Renderer _renderer;
+        private Material _witheredMaterial;
+
+        #endregion
+
+        #region Functions
+
+        // Specific components can be sent down from the State Machine Controller to the States.
+        public FlowerWitheredState(FlowerStateMachine flowerSM, Renderer renderer, Material witheredMaterial)
+        {
+            _flowerSM = flowerSM;
+            _renderer = renderer;
+            _witheredMaterial = witheredMaterial;
+        }
+
+        // This functions marks the start of a new State and is automatically called by the State Machine.
+        public void Enter()
+        {
+            // Switches out materials on State Change.
+            Renderer copyOfRenderer = _renderer;
+            Material[] copiedMaterials = copyOfRenderer.materials;
+            copiedMaterials[0] = _witheredMaterial;
+            copyOfRenderer.materials = copiedMaterials;
+        }
+
+        // This functions marks the end of the current State and is automatically called by the State Machine.
+        public void Exit()
+        {
+
+        }
+
+        // Allows simulation of FixedUpdate() method without a MonoBehaviour attached.
+        public void FixedTick()
+        {
+
+        }
+        
+        // Allows simulation of Update() method without a MonoBehaviour attached.
+        public void Tick()
+        {
+
+        }
+
+        #endregion
+    }
+}
+
