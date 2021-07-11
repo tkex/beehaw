e75412d279ec8ad7541935309c2968a835832933
Tobias <tikado@web.de>
2021-07-11 03:26:19 +0200

    feat: add changebehavioronobject and itemexchange scripts

---
+++ Assets/Scripts/BeeApiarium/ChangeBehaviorOnObject.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeBehaviorOnObject : MonoBehaviour
+{
+
+   /// <summary>
+   /// This file contains at the moment no logic.
+   /// But can be used for example in future for example applying a specific behavior
+   /// on specific objects like slow roation or changing colours.
+   /// </summary>
+
+}
